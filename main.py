955beccb72eca940498049dda6b6438e262bfd66
SUNKARA VENKATA NAVEEN <40927874+nexar3697@users.noreply.github.com>
2019-01-20 12:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#Creating Music File Description
+#Creating Music File Description of Taki Taki
+
+#Defining variables as Follows
+Song = "Taki Taki"
+Artist = "DJ Snake"
+Featured_Artists = "Salena Gomez,Ozuna,cardi B"
+Genre = "Dance/Electronic"
+DurationInMin = "3.25 Min"
+Released = 2018
+#End of the value assign to variables 
+
+#Printing The variables
+print(Song)
+print(Artist)
+print(Featured_Artists)
+print(Genre)
+print(Released)
+print(DurationInMin)
+
+#Successfully Describe the Music File
